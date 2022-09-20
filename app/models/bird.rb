1682016a9430f74b7c30519d21d185154592df20
Kyle Tillson <kyletillson004@gmail.com>
2022-09-19 19:38:06 -0600

    commit tree 7567894e25a7608fff7ddc98782c978ff2b3f738

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,22 @@
 class Bird < ApplicationRecord
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    private
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    
+    
+    
+    def create
+        render json: bird, status: :created
+    end
+      
+    def update
+        bird = find_bird
+        bird.update!(bird_params)
+        render json: bird
+    end
+
 end
